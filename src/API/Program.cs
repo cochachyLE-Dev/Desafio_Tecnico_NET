a7c985826c9a239d81b4f2e19e3a595b8fc59993
Luis Eduardo Cochachi Chamorro <luis.cochachi.eng@gmail.com>
2023-03-05 11:28:41 -0500

    API

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,25 +1,43 @@
+using API.Domain.Settings;
+using API.Infrastructure.Extension;
+using API.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = builder.Configuration;
+
+builder.Services.AddDbContext(configuration);
+builder.Services.AddIdentityService(configuration);
+builder.Services.AddScopedService();
+builder.Services.AddSwaggerOpenAPI();
+builder.Services.AddServiceLayer();
+//builder.Services.AddFeatureManagement();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+{    
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(options => options.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()); //Replace "*" by origins.
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.ConfigureMiddleware();
+app.ConfigureSwagger();
 
 app.MapControllers();
 
 app.Run();
+
+partial class Program { 
+    public AppSettings AppSettings { get; set; } = new AppSettings();
+}
