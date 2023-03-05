161dfc47fcea6f93eddce08fbba0545e3f195642
Luis Eduardo Cochachi Chamorro <luis.cochachi.eng@gmail.com>
2023-03-04 21:21:33 -0500

    API.Service

--- src/API.Service/DependencyInjection.cs
+++ src/API.Service/DependencyInjection.cs
@@ -1,9 +1,90 @@
-﻿using System;
+﻿using API.Domain.Shared;
+using API.Service.Contract;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
 
 namespace API.Service
 {
-    public class Class1
+    public static class DependencyInjection
     {
-
+        public static void AddServiceLayer(this IServiceCollection services)
+        {
+            services.AddMediatR(o => {
+                o.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });            
+        }
+        public static IServiceCollection AddCurrentUserService(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICurrentUserService, ICurrentUserService>();
+            return services;
+        }
+        public static void AddIdentityService(this IServiceCollection services, IConfiguration configuration) {
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o => {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = false;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JWTSettings:Issuer"],
+                    ValidAudience = configuration["JWTSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]!))
+                };
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception is SecurityTokenExpiredException)
+                        {
+                            context.Response.StatusCode = 401;
+                            context.Response.ContentType = "application/json";
+                            context.Response.Headers.Add("Token-Expired", "true");
+                            var result = JsonSerializer.Serialize(Response.Fail(StatusCode.PermissionDenied, "Token Expired"));
+                            return context.Response.WriteAsync(result);
+                        }
+                        else
+                        {
+                            context.NoResult();
+                            context.Response.StatusCode = 500;
+                            context.Response.ContentType = "text/plain";
+                            var result = JsonSerializer.Serialize(Response.Fail(StatusCode.InvalidArgument, context.Exception.ToString()));
+                            return context.Response.WriteAsync(result);
+                        }
+                    },
+                    OnChallenge = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.StatusCode = 401;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonSerializer.Serialize(Response.Fail(StatusCode.Unauthenticated, "You are not Authorized"));
+                        return context.Response.WriteAsync(result);
+                    },
+                    OnForbidden = context =>
+                    {
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonSerializer.Serialize(Response.Fail(StatusCode.PermissionDenied, "You are not authorized to access this resource"));
+                        return context.Response.WriteAsync(result);
+                    }
+                };
+            });
+        }
     }
 }
